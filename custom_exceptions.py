47dae3e2cf36276bb7fc68fb0437cc57a7552a3c
kolchanovaa <fdf>
2022-02-22 12:33:28 +0500

    Exceptions, multiple params

---
+++ custom_exceptions.py
@@ -0,0 +1,18 @@
+class ServiceNotFound(Exception):
+    pass
+
+
+class ParamNotFound(Exception):
+    pass
+
+
+class RequireParamNotSet(Exception):
+    pass
+
+
+class ServiceMethodNotAllowed(Exception):
+    pass
+
+
+class MethodNotFound(Exception):
+    pass
--- test.py
+++ test.py
@@ -1,9 +1,30 @@
 from main import ApiSync
-api_client = ApiSync('GPSPROGR')
+import custom_exceptions
+
+api_client = ApiSync('test_service', is_sync_get_schema=True)
 
 
 def test_send_request():
-    api_client.send_request_api('newuser', {'fio': "12312", 'organizations': ['111', '111']}, False)
+    res = api_client.send_request_api('getApiStruct', {'format': "json"}, 'API', False)
+    assert res == api_client.schema
 
-def test_wrond_type():
-    api_client.send_request_api('newuser', {'fio': 123, 'organizations': ['111', '111']}, False)
+
+def test_wrong_params():
+    try:
+        api_client.send_request_api('newuser', {'fio': "12312", 'organizations': ['111', '111']}, 'API', False)
+    except custom_exceptions.MethodNotFound:
+        assert True
+
+
+def test_multiple_data_send():
+    res = api_client.send_request_api('getApiStruct', [{'format': "json"}, {'format': "json"}], 'API', False)
+    assert res
+
+
+def test_multiple_data_send_fail():
+    try:
+        res = api_client.send_request_api('getApiStruct', [{'format': "json"},
+                                                           {'format': 'json', 'newuser': "json"}],
+                                          'API', False)
+    except custom_exceptions.MethodNotFound:
+        assert True
