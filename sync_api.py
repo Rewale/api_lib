8698e8a975eea8fc289249d95e2b44d07db671bc
kolchanovaa <fdf>
2022-05-05 10:10:30 +0500

    Fix schema

--- sync_api.py
+++ sync_api.py
@@ -66,7 +66,7 @@ class ApiSync:
         # Для тестов можно загружать словарь
         self.schema = None
         if schema is not None:
-            self.schema = schem
+            self.schema = schema
         self.get_schema_sync()
         self.logger = rabbit_logger
         check_methods_handlers(self.schema[self.service_name], methods)
